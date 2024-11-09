29944171b020efae609cf0c6b43d9d53ff9f9a4a
rodolfoinfantini <rodolfo.infantini@gmail.com>
2024-11-09 02:00:53 -0300

    feat(servidor): read ISO DATE TIME format

--- servidor/src/main/json/messages/entities/BalanceChange.java
+++ servidor/src/main/json/messages/entities/BalanceChange.java
@@ -1,6 +1,7 @@
 package main.json.messages.entities;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BalanceChange {
     private double value;
@@ -19,11 +20,7 @@ public class BalanceChange {
     }
 
     public LocalDateTime getCreatedAt() {
-        // yyyy-MM-dd
-        final var year = Integer.parseInt(createdAt.substring(0, 4));
-        final var month = Integer.parseInt(createdAt.substring(5, 7));
-        final var day = Integer.parseInt(createdAt.substring(8, 10));
-        return LocalDateTime.of(year, month, day, 0, 0);
+        return LocalDateTime.parse(createdAt, DateTimeFormatter.ISO_DATE_TIME);
     }
 
     @Override
