bb4761f03046ff3f11420137333dbba96919d47d
rodolfoinfantini <rodolfo.infantini@gmail.com>
2024-11-25 01:43:53 -0300

    feat(backend): adding 404 support

--- backend/routers/routers.js
+++ backend/routers/routers.js
@@ -13,8 +13,16 @@ export default function initRoutes(app) {
     app.use('/balance', balanceRouter)
     app.use('/event', eventRouter)
 
+    app.get('*', (_, res) => {
+        res.status(404).send('Not Found');
+    });
+
     app.use((err, _, res) => {
         console.error(err)
-        res.status(500).json({ message: 'Something went wrong' })
+        try {
+            res.status(500).json({ message: 'Something went wrong' })
+        } catch (error) {
+            console.error(error)
+        }
     })
 }
