1ec9c4f39c38acee41a2429b60129125f2723cdf
rodolfoinfantini <rodolfo.infantini@gmail.com>
2024-11-25 01:43:39 -0300

    fix(backend): mysql port formatting

--- backend/data/connection.js
+++ backend/data/connection.js
@@ -7,5 +7,5 @@ export const sequelize = new Sequelize({
     user: process.env.DB_USER ?? 'root',
     password: process.env.DB_PASS ?? 'root',
     host: process.env.DB_HOST ?? 'localhost',
-    port: process.env.DB_PORT ?? 3306,
+    port: +(process.env.DB_PORT ?? 3306),
 })
